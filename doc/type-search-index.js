952fcf58e0e9a6cadac895be11c703402efc179f
Gowrav Kumar <83865815+Gowrav-Kumar@users.noreply.github.com>
2024-06-09 16:01:31 +0530

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"assignment","l":"Organization"},{"p":"assignment","l":"PlacementProcess"},{"p":"assignment","l":"PlacementProcessDriver"},{"p":"assignment","l":"Student"}];updateSearchResults();
